11f5e97e90a5c8321ebbe09fcc513f93b36c8678
gbauermann <gabriel.bauermann@gmail.com>
2019-04-14 20:28:56 -0300

    Add files via upload

--- teste/Scripts/script.js
+++ teste/Scripts/script.js
@@ -1,97 +1,98 @@
-
+//alterar aqui o endereço da web api
 const wsUrl = 'https://localhost:44380/api/customer'
+
 getCustomers();
 
-$(document).ready(function(){
-	// Activate tooltip
-	$('[data-toggle="tooltip"]').tooltip();	
+$(document).ready(function () {
+    // Activate tooltip
+    $('[data-toggle="tooltip"]').tooltip();
 });
 
-function getCustomers(){
-    var tApp = 
-    angular.
-    module("tModule", []);
+function getCustomers() {
+    var tApp =
+        angular.
+            module("tModule", []);
 
-    tApp.controller("tController", function($scope, $http) {
-        $http.get(wsUrl).then(function(response) {
+    tApp.controller("tController", function ($scope, $http) {
+        $http.get(wsUrl).then(function (response) {
             $scope.customers = response.data;
         });
-        
-        $scope.showCustomer = function(nForm, customer) {  
+
+        $scope.showCustomer = function (nForm, customer) {
 
             $('#' + nForm + 'Name').val(customer.tradingName);
             $('#' + nForm + 'CoName').val(customer.companyName);
             $('#' + nForm + 'Email').val(customer.email);
             $('#' + nForm + 'Cnpj').val(customer.cnpj);
             $('#' + nForm + 'Phone').val(customer.phoneNumber);
-            $('#' + nForm + 'Address').val(customer.address);   
-            
-            $(".modal-footer #customerId").val( customer.id );
-        };   
+            $('#' + nForm + 'Address').val(customer.address);
 
-        
+            $(".modal-footer #customerId").val(customer.id);
+        };
+
+
     });
 }
 
-function editCustomer(){
+function editCustomer() {
 
     var cId = $(".modal-footer #customerId").val();
-    var data = 
-        {
-            Id: cId,
-            TradingName: $("#editName").val(), 
-            CompanyName: $("#editCoName").val(),
-            Email: $("#editEmail").val(),
-            Address: $("#editAddress").val(),
-            CNPJ: $("#editCnpj").val(),
-            PhoneNumber: $("#editPhone").val()
-       };
+    var data =
+    {
+        Id: cId,
+        TradingName: $("#editName").val(),
+        CompanyName: $("#editCoName").val(),
+        Email: $("#editEmail").val(),
+        Address: $("#editAddress").val(),
+        CNPJ: $("#editCnpj").val(),
+        PhoneNumber: $("#editPhone").val()
+    };
 
     $.ajax({
         type: "PUT",
         url: wsUrl + '/' + cId,
         data: data,
-        success: function(response){
-            location.reload();   
+        success: function (response) {
+            location.reload();
         }
-});
+    });
 
 }
 
-function newCustomer(){
+function newCustomer() {
 
-    $.post(wsUrl, {  TradingName: $("#newName").val(), 
-                     CompanyName: $("#newCoName").val(),
-                     Email: $("#newEmail").val(),
-                     Address: $("#newAddress").val(),
-                     CNPJ: $("#newCnpj").val(),
-                     PhoneNumber: $("#newPhone").val()
-                    })
-            .done(function(data){                    
-                $('#addCustomerModal').modal('hide');    
-                location.reload();                                         
+    $.post(wsUrl, {
+        TradingName: $("#newName").val(),
+        CompanyName: $("#newCoName").val(),
+        Email: $("#newEmail").val(),
+        Address: $("#newAddress").val(),
+        CNPJ: $("#newCnpj").val(),
+        PhoneNumber: $("#newPhone").val()
+    })
+        .done(function (data) {
+            $('#addCustomerModal').modal('hide');
+            location.reload();
         });
-               
+
 
 }
 
-function deleteCustomer(){
-       
+function deleteCustomer() {
+
     var cId = $(".modal-body #customerId").val();
-   
+
     $.ajax({
         url: wsUrl + '/' + cId,
         type: 'DELETE',
-        success: function(result) {
-            location.reload(); 
+        success: function (result) {
+            location.reload();
         }
-    });    
+    });
 }
 
 $(document).on("click", ".open-deleteCustomerModal", function () {
     var customerId = $(this).data('id');
-    $(".modal-body #customerId").val( customerId );
-    
+    $(".modal-body #customerId").val(customerId);
+
 });
 
-   
